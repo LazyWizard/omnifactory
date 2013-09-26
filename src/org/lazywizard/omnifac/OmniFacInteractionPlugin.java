5c9412b9b316f4de6444d8b8f9a9ecc3a740bcfd
LazyWizard <iu65a@hotmail.com>
2013-09-25 22:37:58 -0500

    tmp

---
+++ src/org/lazywizard/omnifac/OmniFacInteractionPlugin.java
@@ -0,0 +1,48 @@
+package org.lazywizard.omnifac;
+
+import com.fs.starfarer.api.campaign.InteractionDialogAPI;
+import com.fs.starfarer.api.campaign.InteractionDialogPlugin;
+import com.fs.starfarer.api.combat.EngagementResultAPI;
+
+public class OmniFacInteractionPlugin implements InteractionDialogPlugin
+{
+    public OmniFacInteractionPlugin()
+    {
+    }
+
+    @Override
+    public void init(InteractionDialogAPI dialog)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void optionSelected(String optionText, Object optionData)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void optionMousedOver(String optionText, Object optionData)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void advance(float amount)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void backFromEngagement(EngagementResultAPI battleResult)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getContext()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}

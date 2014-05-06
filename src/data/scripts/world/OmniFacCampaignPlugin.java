0736fed08ab219c810f146a9f0347fe34c878bc1
LazyWizard <iu65a@hotmail.com>
2014-05-06 17:57:55 -0500

    Code cleanup

---
+++ src/data/scripts/world/OmniFacCampaignPlugin.java
@@ -0,0 +1,27 @@
+package data.scripts.world;
+
+import com.fs.starfarer.api.PluginPick;
+import com.fs.starfarer.api.campaign.BaseCampaignPlugin;
+import com.fs.starfarer.api.campaign.InteractionDialogPlugin;
+import com.fs.starfarer.api.campaign.SectorEntityToken;
+
+class OmniFacCampaignPlugin extends BaseCampaignPlugin
+{
+    @Override
+    public boolean isTransient()
+    {
+       return true;
+    }
+
+    @Override
+    public PluginPick<InteractionDialogPlugin> pickInteractionDialogPlugin(SectorEntityToken interactionTarget)
+    {
+        if (OmniFac.isFactory(interactionTarget))
+        {
+            // TODO: Add custom interaction dialog
+            return null;
+        }
+
+        return null;
+    }
+}
--- src/data/scripts/world/OmniFacSettings.java
+++ src/data/scripts/world/OmniFacSettings.java
@@ -2,157 +2,133 @@ package data.scripts.world;
 
 import com.fs.starfarer.api.Global;
 import java.io.IOException;
-import org.apache.log4j.Level;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class OmniFacSettings
 {
-    public boolean SHOW_ADDED_CARGO = false;
-    public boolean SHOW_ANALYSIS_COMPLETE = true;
-    public boolean SHOW_LIMIT_REACHED = true;
-    public boolean REMOVE_BROKEN_GOODS = false;
-    public float SHIP_ANALYSIS_TIME_MOD = 0.5f;
-    public float WEAPON_ANALYSIS_TIME_MOD = 0.5f;
-    public float SHIP_PRODUCTION_TIME_MOD = 1.0f;
-    public float WEAPON_PRODUCTION_TIME_MOD = 1.0f;
-    public int REQUIRED_CREW = 0;
-    public float REQUIRED_SUPPLIES_PER_DAY = 0f;
-    public float REQUIRED_FUEL_PER_DAY = 0f;
-    public int MAX_HULLS_PER_FIGHTER = 3;
-    public int MAX_HULLS_PER_FRIGATE = 3;
-    public int MAX_HULLS_PER_DESTROYER = 2;
-    public int MAX_HULLS_PER_CRUISER = 2;
-    public int MAX_HULLS_PER_CAPITAL = 1;
-    public float MAX_STACKS_PER_WEAPON = 0.5f;
+    private final boolean showAddedCargo;
+    private final boolean showAnalysisComplete;
+    private final boolean showLimitReached;
+    private final boolean removeBrokenGoods;
+    private final float shipAnalysisTimeMod;
+    private final float weaponAnalysisTimeMod;
+    private final float shipProductionTimeMod;
+    private final float weaponProductionTimeMod;
+    private final int requiredCrew;
+    private final float requiredSuppliesPerDay;
+    private final float requiredFuelPerDay;
+    private final int maxHullsPerFighter;
+    private final int maxHullsPerFrigate;
+    private final int maxHullsPerDestroyer;
+    private final int maxHullsPerCruiser;
+    private final int maxHullsPerCapital;
+    private final float maxStacksPerWeapon;
 
-    OmniFacSettings(String settingsFile)
+    OmniFacSettings(String settingsFile) throws JSONException, IOException
     {
-        reloadSettingsFromJSON(settingsFile);
+        JSONObject settings = Global.getSettings().loadJSON(settingsFile);
+        showAddedCargo = settings.getBoolean("showAddedCargo");
+        showAnalysisComplete = settings.getBoolean("showAnalysisComplete");
+        showLimitReached = settings.getBoolean("showLimitReached");
+        removeBrokenGoods = settings.getBoolean("removeBrokenGoods");
+        shipAnalysisTimeMod = (float) settings.getDouble("shipAnalysisTimeMod");
+        weaponAnalysisTimeMod = (float) settings.getDouble("weaponAnalysisTimeMod");
+        shipProductionTimeMod = (float) settings.getDouble("shipProductionTimeMod");
+        weaponProductionTimeMod = (float) settings.getDouble("weaponProductionTimeMod");
+        requiredCrew = settings.getInt("requiredCrewToFunction");
+        requiredSuppliesPerDay = (float) settings.getDouble("requiredSuppliesPerDay");
+        requiredFuelPerDay = (float) settings.getDouble("requiredFuelPerDay");
+        maxHullsPerFighter = settings.getInt("maxHullsPerFighter");
+        maxHullsPerFrigate = settings.getInt("maxHullsPerFrigate");
+        maxHullsPerDestroyer = settings.getInt("maxHullsPerDestroyer");
+        maxHullsPerCruiser = settings.getInt("maxHullsPerCruiser");
+        maxHullsPerCapital = settings.getInt("maxHullsPerCapital");
+        maxStacksPerWeapon = (float) settings.getDouble("maxStacksPerWeapon");
     }
 
-    public void reloadSettingsFromJSON(String filePath)
+    public boolean shouldShowAddedCargo()
     {
-        try
-        {
-            JSONObject settings = Global.getSettings().loadJSON(filePath);
-            SHOW_ADDED_CARGO = settings.getBoolean("showAddedCargo");
-            SHOW_ANALYSIS_COMPLETE = settings.getBoolean("showAnalysisComplete");
-            SHOW_LIMIT_REACHED = settings.getBoolean("showLimitReached");
-            REMOVE_BROKEN_GOODS = settings.getBoolean("removeBrokenGoods");
-            SHIP_ANALYSIS_TIME_MOD = (float) settings.getDouble("shipAnalysisTimeMod");
-            WEAPON_ANALYSIS_TIME_MOD = (float) settings.getDouble("weaponAnalysisTimeMod");
-            SHIP_PRODUCTION_TIME_MOD = (float) settings.getDouble("shipProductionTimeMod");
-            WEAPON_PRODUCTION_TIME_MOD = (float) settings.getDouble("weaponProductionTimeMod");
-            REQUIRED_CREW = settings.getInt("requiredCrewToFunction");
-            REQUIRED_SUPPLIES_PER_DAY = (float) settings.getDouble("requiredSuppliesPerDay");
-            REQUIRED_FUEL_PER_DAY = (float) settings.getDouble("requiredFuelPerDay");
-            MAX_HULLS_PER_FIGHTER = settings.getInt("maxHullsPerFighter");
-            MAX_HULLS_PER_FRIGATE = settings.getInt("maxHullsPerFrigate");
-            MAX_HULLS_PER_DESTROYER = settings.getInt("maxHullsPerDestroyer");
-            MAX_HULLS_PER_CRUISER = settings.getInt("maxHullsPerCruiser");
-            MAX_HULLS_PER_CAPITAL = settings.getInt("maxHullsPerCapital");
-            MAX_STACKS_PER_WEAPON = (float) settings.getDouble("maxStacksPerWeapon");
-        }
-        catch (IOException ex)
-        {
-            Global.getLogger(OmniFacSettings.class).log(Level.ERROR,
-                    "Failed to load " + filePath, ex);
-            return;
-        }
-        catch (JSONException ex)
-        {
-            Global.getLogger(OmniFacSettings.class).log(Level.ERROR,
-                    "Failed to parse " + filePath, ex);
-            return;
-        }
-
-        Global.getLogger(OmniFacSettings.class).log(Level.INFO,
-                "Loaded settings successfully");
+        return showAddedCargo;
     }
 
-    public void setShowAddedCargo(boolean showAddedCargo)
+    public boolean shouldShowAnalysisComplete()
     {
-        SHOW_ADDED_CARGO = showAddedCargo;
+        return showAnalysisComplete;
     }
 
-    public void setShowLimitReached(boolean showLimitReached)
+    public boolean shouldShowLimitReached()
     {
-        SHOW_LIMIT_REACHED = showLimitReached;
+        return showLimitReached;
     }
 
-    public void setShowAnalysisComplete(boolean showAnalysisComplete)
+    public boolean shouldRemoveBrokenGoods()
     {
-        SHOW_ANALYSIS_COMPLETE = showAnalysisComplete;
+        return removeBrokenGoods;
     }
 
-    public void setRemoveBrokenGoods(boolean removeBrokenGoods)
+    public float getShipAnalysisTimeMod()
     {
-        REMOVE_BROKEN_GOODS = removeBrokenGoods;
+        return shipAnalysisTimeMod;
     }
 
-    public void setShipAnalysisTimeModifier(float modifier)
+    public float getWeaponAnalysisTimeMod()
     {
-        SHIP_ANALYSIS_TIME_MOD = modifier;
+        return weaponAnalysisTimeMod;
     }
 
-    public void setWeaponAnalysisTimeModifier(float modifier)
+    public float getShipProductionTimeMod()
     {
-        WEAPON_ANALYSIS_TIME_MOD = modifier;
+        return shipProductionTimeMod;
     }
 
-    public void setShipProductionTimeModifier(float modifier)
+    public float getWeaponProductionTimeMod()
     {
-        SHIP_PRODUCTION_TIME_MOD = modifier;
+        return weaponProductionTimeMod;
     }
 
-    public void setWeaponProductionTimeModifier(float modifier)
+    public int getRequiredCrew()
     {
-        WEAPON_PRODUCTION_TIME_MOD = modifier;
+        return requiredCrew;
     }
 
-    public void setRequiredCrew(int requiredCrew)
+    public float getRequiredSuppliesPerDay()
     {
-        REQUIRED_CREW = requiredCrew;
+        return requiredSuppliesPerDay;
     }
 
-    public void setRequiredSuppliesPerDay(float suppliesPerDay)
+    public float getRequiredFuelPerDay()
     {
-        REQUIRED_SUPPLIES_PER_DAY = suppliesPerDay;
+        return requiredFuelPerDay;
     }
 
-    public void setRequiredFuelPerDay(float fuelPerDay)
+    public int getMaxHullsPerFighter()
     {
-        REQUIRED_FUEL_PER_DAY = fuelPerDay;
+        return maxHullsPerFighter;
     }
 
-    public void setMaxHullsPerFighter(int maxHulls)
+    public int getMaxHullsPerFrigate()
     {
-        MAX_HULLS_PER_FIGHTER = maxHulls;
+        return maxHullsPerFrigate;
     }
 
-    public void setMaxHullsPerFrigate(int maxHulls)
+    public int getMaxHullsPerDestroyer()
     {
-        MAX_HULLS_PER_FRIGATE = maxHulls;
+        return maxHullsPerDestroyer;
     }
 
-    public void setMaxHullsPerDestroyer(int maxHulls)
+    public int getMaxHullsPerCruiser()
     {
-        MAX_HULLS_PER_DESTROYER = maxHulls;
+        return maxHullsPerCruiser;
     }
 
-    public void setMaxHullsPerCruiser(int maxHulls)
+    public int getMaxHullsPerCapital()
     {
-        MAX_HULLS_PER_CRUISER = maxHulls;
+        return maxHullsPerCapital;
     }
 
-    public void setMaxHullsPerCapital(int maxHulls)
+    public float getMaxStacksPerWeapon()
     {
-        MAX_HULLS_PER_CAPITAL = maxHulls;
-    }
-
-    public void setMaxStacksPerWeapon(float maxStacks)
-    {
-        MAX_STACKS_PER_WEAPON = maxStacks;
+        return maxStacksPerWeapon;
     }
 }

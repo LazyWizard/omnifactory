952dcb5027d784e410533aea0618b5c5a49c343a
LazyWizard <iu65a@hotmail.com>
2015-01-09 20:48:28 -0600

    Added storage tab

--- src/org/lazywizard/omnifac/OmniFacModPlugin.java
+++ src/org/lazywizard/omnifac/OmniFacModPlugin.java
@@ -1,5 +1,7 @@
 package org.lazywizard.omnifac;
 
+import java.util.Collections;
+import java.util.List;
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.SectorAPI;
@@ -7,42 +9,20 @@ import com.fs.starfarer.api.campaign.SectorEntityToken;
 import com.fs.starfarer.api.campaign.StarSystemAPI;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.impl.campaign.ids.Conditions;
+import com.fs.starfarer.api.impl.campaign.ids.Submarkets;
 import com.fs.starfarer.api.impl.campaign.shared.SharedData;
+import com.fs.starfarer.api.impl.campaign.submarkets.StoragePlugin;
+import org.apache.log4j.Level;
 
 public class OmniFacModPlugin extends BaseModPlugin
 {
-    private static void initOmniFac()
+    public static void initOmnifactory(SectorEntityToken factory)
     {
-        SectorEntityToken station = null;
-        SectorAPI sector = Global.getSector();
-        StarSystemAPI system = sector.getStarSystem("corvus");
-        if (system != null)
+        // Only one controller script per factory
+        if (OmniFac.isFactory(factory))
         {
-            // By default the Omnifactory orbits the planet Somnus in the Corvus system
-            station = system.addOrbitalStation(Constants.STATION_ID,
-                    system.getEntityById("corvus_IV"), 315f,
-                    300f, 50f, Constants.STATION_NAME, Constants.STATION_FACTION);
-        }
-        else
-        {
-            // For total conversions, orbit the first available star
-            for (StarSystemAPI tmp : sector.getStarSystems())
-            {
-                if (tmp.getStar() != null)
-                {
-                    system = tmp;
-                    station = system.addOrbitalStation(Constants.STATION_ID,
-                            system.getStar(), 315f, system.getStar().getRadius() * 1.5f,
-                            50f, Constants.STATION_NAME, Constants.STATION_FACTION);
-                    break;
-                }
-            }
-        }
-
-        // In the unlikely situation where all stars in the universe somehow vanish...
-        if (system == null || station == null)
-        {
-            throw new RuntimeException("Could not find a valid Omnifactory location!");
+            throw new RuntimeException(factory.getFullName()
+                    + " is already an Omnifactory!");
         }
 
         // Set up market data for the Omnifactory
@@ -50,15 +30,50 @@ public class OmniFacModPlugin extends BaseModPlugin
                 Constants.STATION_ID, Constants.STATION_NAME, 0);
         SharedData.getData().getMarketsWithoutPatrolSpawn().add(Constants.STATION_ID);
         SharedData.getData().getMarketsWithoutTradeFleetSpawn().add(Constants.STATION_ID);
-        market.setPrimaryEntity(station);
+        market.setPrimaryEntity(factory);
         market.setFactionId(Constants.STATION_FACTION);
         market.addCondition(Conditions.ABANDONED_STATION);
         market.addSubmarket(Constants.SUBMARKET_ID);
-        station.setMarket(market);
+        market.addSubmarket(Submarkets.SUBMARKET_STORAGE);
+        ((StoragePlugin) market.getSubmarket(Submarkets.SUBMARKET_STORAGE)
+                .getPlugin()).setPlayerPaidToUnlock(true);
+        factory.setMarket(market);
 
-        // Add the Omnifactory controller script to the station
-        OmniFac factory = new OmniFac(station);
-        system.addScript(factory);
+        // Add the Omnifactory controller script
+        OmniFac facScript = new OmniFac(factory);
+        factory.getContainingLocation().addScript(facScript);
+    }
+
+    private static SectorEntityToken createOmnifactory()
+    {
+        SectorAPI sector = Global.getSector();
+        StarSystemAPI corvus = sector.getStarSystem("corvus");
+        if (corvus != null)
+        {
+            // By default the Omnifactory orbits the planet Somnus in the Corvus system
+            return corvus.addOrbitalStation(Constants.STATION_ID,
+                    corvus.getEntityById("corvus_IV"), 315f,
+                    300f, 50f, Constants.STATION_NAME, Constants.STATION_FACTION);
+        }
+
+        // For total conversions, orbit a random star
+        List<StarSystemAPI> systems = sector.getStarSystems();
+        Collections.shuffle(systems);
+        for (StarSystemAPI system : systems)
+        {
+            if (system.getStar() != null)
+            {
+                Global.getLogger(OmniFacModPlugin.class).log(Level.INFO,
+                        "Omnifactory random starting location: orbiting "
+                        + system.getBaseName() + "'s star");
+                return system.addOrbitalStation(Constants.STATION_ID,
+                        system.getStar(), 315f, (system.getStar().getRadius() * 1.5f)
+                        + 50f, 50f, Constants.STATION_NAME, Constants.STATION_FACTION);
+            }
+        }
+
+        // In the unlikely situation where all stars in the universe somehow vanish...
+        throw new RuntimeException("Could not find a valid Omnifactory location!");
     }
 
     @Override
@@ -72,7 +87,7 @@ public class OmniFacModPlugin extends BaseModPlugin
     {
         if (!wasEnabledBefore)
         {
-            initOmniFac();
+            initOmnifactory(createOmnifactory());
         }
     }
 }

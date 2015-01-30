973812bd1573616cdb847caf93e1eb506c8d355c
LazyWizard <iu65a@hotmail.com>
2015-01-29 21:23:05 -0600

    Added random starting location option, polished random code

--- src/org/lazywizard/omnifac/OmniFacModPlugin.java
+++ src/org/lazywizard/omnifac/OmniFacModPlugin.java
@@ -1,18 +1,26 @@
 package org.lazywizard.omnifac;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.OrbitAPI;
+import com.fs.starfarer.api.campaign.PlanetAPI;
 import com.fs.starfarer.api.campaign.SectorAPI;
 import com.fs.starfarer.api.campaign.SectorEntityToken;
 import com.fs.starfarer.api.campaign.StarSystemAPI;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.impl.campaign.ids.Conditions;
 import com.fs.starfarer.api.impl.campaign.ids.Submarkets;
+import com.fs.starfarer.api.impl.campaign.ids.Tags;
 import com.fs.starfarer.api.impl.campaign.shared.SharedData;
 import com.fs.starfarer.api.impl.campaign.submarkets.StoragePlugin;
 import org.apache.log4j.Level;
+import org.lazywizard.lazylib.CollectionUtils;
+import org.lazywizard.lazylib.CollectionUtils.CollectionFilter;
 
 public class OmniFacModPlugin extends BaseModPlugin
 {
@@ -44,35 +52,77 @@ public class OmniFacModPlugin extends BaseModPlugin
         factory.getContainingLocation().addScript(facScript);
     }
 
+    /*public static void testRandomLocations(int numTests)
+     {
+     for (int x = 0; x < numTests; x++)
+     {
+     SectorEntityToken omnifac = createOmnifactory();
+     omnifac.getContainingLocation().removeEntity(omnifac);
+     }
+     }*/
     private static SectorEntityToken createOmnifactory()
     {
         SectorAPI sector = Global.getSector();
-        StarSystemAPI corvus = sector.getStarSystem("corvus");
-        if (corvus != null)
+
+        if (!OmniFacSettings.shouldHaveRandomStartingLocation())
         {
-            // By default the Omnifactory orbits the planet Somnus in the Corvus system
-            return corvus.addOrbitalStation(Constants.STATION_ID,
-                    corvus.getEntityById("corvus_IV"), 315f,
-                    300f, 50f, Constants.STATION_NAME, Constants.STATION_FACTION);
+            StarSystemAPI corvus = sector.getStarSystem("corvus");
+            if (corvus != null)
+            {
+                Global.getLogger(OmniFacModPlugin.class).log(Level.INFO,
+                        "Omnifactory starting location: orbiting Somnus in Corvus");
+
+                // By default the Omnifactory orbits the planet Somnus in the Corvus system
+                return corvus.addOrbitalStation(Constants.STATION_ID,
+                        corvus.getEntityById("corvus_IV"), 315f,
+                        300f, 50f, Constants.STATION_NAME, Constants.STATION_FACTION);
+            }
+
+            Global.getLogger(OmniFacModPlugin.class).log(Level.INFO,
+                    "Corvus not found, using random Omnifactory location");
         }
 
-        // For total conversions, orbit a random star
-        List<StarSystemAPI> systems = sector.getStarSystems();
+        // Find a random planet or star that doesn't already have a station
+        List<StarSystemAPI> systems = new ArrayList<>(sector.getStarSystems());
+        Collections.shuffle(systems);
+        for (StarSystemAPI system : systems)
+        {
+            CollectionFilter planetFilter = new EmptyOrbitFilter(system);
+            List<PlanetAPI> planets = CollectionUtils.filter(
+                    system.getPlanets(), planetFilter);
+            if (!planets.isEmpty())
+            {
+                Collections.shuffle(planets);
+                PlanetAPI toOrbit = planets.get(0);
+
+                Global.getLogger(OmniFacModPlugin.class).log(Level.INFO,
+                        "Omnifactory starting location: orbiting "
+                        + toOrbit.getName() + " in " + system.getBaseName());
+
+                return system.addOrbitalStation(Constants.STATION_ID, toOrbit,
+                        (float) (Math.random() * 360f), toOrbit.getRadius() + 150f,
+                        50f, Constants.STATION_NAME, Constants.STATION_FACTION);
+            }
+        }
+
+        // No empty planets found? Orbit a random star
         Collections.shuffle(systems);
         for (StarSystemAPI system : systems)
         {
             if (system.getStar() != null)
             {
                 Global.getLogger(OmniFacModPlugin.class).log(Level.INFO,
-                        "Omnifactory random starting location: orbiting "
+                        "Omnifactory starting location: orbiting "
                         + system.getBaseName() + "'s star");
                 return system.addOrbitalStation(Constants.STATION_ID,
-                        system.getStar(), 315f, (system.getStar().getRadius() * 1.5f)
-                        + 50f, 50f, Constants.STATION_NAME, Constants.STATION_FACTION);
+                        system.getStar(), (float) (Math.random() * 360f),
+                        (system.getStar().getRadius() * 1.5f) + 50f, 50f,
+                        Constants.STATION_NAME, Constants.STATION_FACTION);
             }
         }
 
-        // In the unlikely situation where all stars in the universe somehow vanish...
+        // In the unlikely situation where every planet's orbit is occupied
+        // and all stars in the sector have somehow vanished...
         throw new RuntimeException("Could not find a valid Omnifactory location!");
     }
 
@@ -88,6 +138,30 @@ public class OmniFacModPlugin extends BaseModPlugin
         if (!wasEnabledBefore)
         {
             initOmnifactory(createOmnifactory());
+        }
+    }
+
+    private static class EmptyOrbitFilter implements CollectionFilter<SectorEntityToken>
+    {
+        final Set<SectorEntityToken> blocked;
+
+        private EmptyOrbitFilter(StarSystemAPI system)
+        {
+            blocked = new HashSet<>();
+            for (SectorEntityToken station : system.getEntitiesWithTag(Tags.STATION))
+            {
+                OrbitAPI orbit = station.getOrbit();
+                if (orbit != null && orbit.getFocus() != null)
+                {
+                    blocked.add(station.getOrbit().getFocus());
+                }
+            }
+        }
+
+        @Override
+        public boolean accept(SectorEntityToken token)
+        {
+            return !blocked.contains(token);
         }
     }
 }

4cb357803af6787cc31cc898132e7183aa0ba8cf
Robert Maguire <LazyWizard@users.noreply.github.com>
2015-02-04 01:44:37 -0600

    Added console command to get omnifactory data

---
+++ src/org/lazywizard/omnifac/commands/OmnifacStatus.java
@@ -0,0 +1,49 @@
+package org.lazywizard.omnifac.commands;
+
+import java.util.List;
+import org.lazywizard.console.BaseCommand;
+import org.lazywizard.console.CommonStrings;
+import org.lazywizard.console.Console;
+import org.lazywizard.lazylib.CollectionUtils;
+import org.lazywizard.lazylib.StringUtils;
+import org.lazywizard.omnifac.OmniFac;
+
+public class OmnifacStatus implements BaseCommand
+{
+    @Override
+    public CommandResult runCommand(String args, CommandContext context)
+    {
+        if (context != CommandContext.CAMPAIGN_MAP)
+        {
+            Console.showMessage(CommonStrings.ERROR_CAMPAIGN_ONLY);
+            return CommandResult.WRONG_CONTEXT;
+        }
+
+        List<OmniFac> factories = OmniFac.getAllFactories();
+        if (factories.isEmpty())
+        {
+            Console.showMessage("There are no active Omnifactories in this save!");
+            return CommandResult.SUCCESS;
+        }
+
+        final boolean showDetailed = "detailed".equals(args);
+        final int lineLength = Console.getSettings().getMaxOutputLineLength() - 3;
+        for (OmniFac fac : factories)
+        {
+            Console.showMessage("Factory " + fac.getLocationString());
+
+            // TODO: Format this to look good
+            if (showDetailed)
+            {
+                Console.showMessage(StringUtils.indent(StringUtils.wrapString("Known ships: "
+                        + CollectionUtils.implode(fac.getKnownShips()), lineLength), "  "));
+                Console.showMessage(StringUtils.indent(StringUtils.wrapString("Known wings: "
+                        + CollectionUtils.implode(fac.getKnownWings()), lineLength), "  "));
+                Console.showMessage(StringUtils.indent(StringUtils.wrapString("Known weapons: "
+                        + CollectionUtils.implode(fac.getKnownWeapons()), lineLength), "  "));
+            }
+        }
+
+        return CommandResult.SUCCESS;
+    }
+}
